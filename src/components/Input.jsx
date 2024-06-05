e83267eb16dd933b7b0becede4d24560488d6766
rudsa <rudsa@naver.com>
2024-06-05 23:38:02 +0900

    tailwind

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,32 +1,22 @@
 import React from "react";
-import { styled } from "styled-components";
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${(props) => (props.$invalid ? "#f87171" : "#6b7280")};
-`;
+export default function Input(props) {
+  let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${(props) => (props.$invalid ? "#fed2d2" : "#d1d5db")};
-  color: ${(props) => (props.$invalid ? "#ef4444" : "#374151")};
-  border: 1px solid ${(props) => (props.$invalid ? "#f73f3f" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
+  let inputClasses = "w-full px-3 py-2 leading-tight border rounded shadow";
 
-export default function CustomInput(props) {
+  if (props.invalid) {
+    labelClasses += " text-red-400";
+    inputClasses += " text-red-500 bg-red-100 border-red-300";
+  } else {
+    labelClasses += " text-stone-400";
+    inputClasses += " text-gray-700 bg-stone-300";
+  }
+
   return (
     <p>
-      <Label $invalid={props.$invalid}>{props.label}</Label>
-      <Input $invalid={props.$invalid} {...props} />
+      <label className={labelClasses}>{props.label}</label>
+      <input className={inputClasses} {...props} />
     </p>
   );
 }
